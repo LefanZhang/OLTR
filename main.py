7722505c402e75bdd7630c054729295b8c355b8b
LefanZhang <lefanchang@gmail.com>
2022-09-06 17:23:30 +0800

    modify imagenet data dir

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ from utils import source_import
 # ================
 # LOAD CONFIGURATIONS
 
-data_root = {'ImageNet': '/home/public/public_dataset/ILSVRC2014/Img',
+data_root = {#'ImageNet': '/home/public/public_dataset/ILSVRC2014/Img',
+             'ImageNet': '/data1/ImageNet',
              'Places': '/home/public/dataset/Places365'}
 
 parser = argparse.ArgumentParser()
